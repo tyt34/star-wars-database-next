2ad7a1a6d6e8e13459157b03e90196422a4ef24c
rnovotochin <rnovotochin@zuzex.com>
2023-06-01 11:01:00 +0300

    добавил компонент Home и Footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from './footer.module.scss'
+
+export default function Footer() {
+  return (
+    <section className={styles.red}>
+      <p> Link on repo this project - </p>
+      <p> Date generated this app.</p>
+    </section>
+  )
+}
---
+++ utils/start-wars.ts
@@ -0,0 +1,28 @@
+const url = 'https://swapi.dev/api/'
+
+export type IDataStarWars = 'things' | 'planets' | 'starships'
+
+export async function getData(
+  typeData: IDataStarWars,
+  number?: string
+) {
+  let urlTypeData = ''
+  if (typeData.toLowerCase() === 'things') {
+    urlTypeData = 'people/'
+  } else if (typeData.toLowerCase() === 'planets') {
+    urlTypeData = 'planets/'
+  } else if (typeData.toLowerCase() === 'starships') {
+    urlTypeData = 'starships/'
+  }
+  urlTypeData =
+    number !== undefined ? urlTypeData + number : urlTypeData
+  let res = await fetch(url + urlTypeData, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+  const json = await res.json()
+  return json
+}
