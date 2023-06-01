102ed89a3b9034fe07b141ac61a2f67542221800
rnovotochin <rnovotochin@zuzex.com>
2023-06-01 10:09:11 +0300

    add

--- next.config.js
+++ next.config.js
@@ -1,4 +1,57 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  /**
+   * При использовании next/imageкомпонента вам нужно будет добавить basePathперед src.
+   * Например, использование /docs/me.pngбудет правильно отображать ваше изображение, если basePathустановлено значение /docs.
+   *
+   * не поддерживается с использованием Turbopack
+   */
+  // basePath: "star-wars",
+  compress: true,
+  devIndicators: {
+    // какое то расхождение с докой
+  },
+  /**
+   * не поддерживается с использованием Turbopack
+   */
+  // distDir: "dist-build",
+  env: {
+    someEnvVarible: "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  /**
+   * exportPathMap устарел,
+   * теперь используется getStaticPaths.
+   * Кажется, надо использовать что-то одно
+   */
+  generateBuildId: async () => {
+    return "v1";
+  },
+  /**
+   * отключение генерации Etags, которая включена по умолчанию
+   */
+  // generateEtags: false,
+  headers: async () => {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "x-custom-header-so",
+            value: "my custom header value",
+          },
+          {
+            key: "x-another-custom-header-so-so",
+            value: "my other custom header value",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
